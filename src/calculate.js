f11245bf22790c8db8102bc796bd4b75bc82506b
ButenkoT <tanya@butenko.me>
2015-11-30 23:10:49 +1100

    main calculation for betting

--- src/calculate.js
+++ src/calculate.js
@@ -1,5 +1,25 @@
-function percentage(){
-
+function normalizeNumber(num){
+  return parseFloat((num).toFixed(2));
 }
 
-exports.percentage = percentage;
+function companyPercentageSum(sum, percent){
+  return normalizeNumber((percent / 100) * sum);
+}
+
+function moneyInPool(sum, percent) {
+  return sum - companyPercentageSum(sum, percent);
+}
+
+function stakeProportion(sum, percent, winnerSteak) {
+  return normalizeNumber(moneyInPool(sum, percent)/winnerSteak );
+}
+
+function sumForPlace(sum) {
+  return normalizeNumber(sum / 3);
+}
+
+exports.normalizeNumber = normalizeNumber;
+exports.companyPercentageSum = companyPercentageSum;
+exports.moneyInPool = moneyInPool;
+exports.stakeProportion = stakeProportion;
+exports.sumForPlace = sumForPlace;
